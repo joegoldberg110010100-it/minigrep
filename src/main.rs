3f63931152e24829800c31b77bd50b8270e45b4e
bypassOS <joegoldberg110010100@gmail.com>
2026-07-20 12:26:19 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+//===================
+use minigrep::Config;
+use minigrep::run;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err|{
@@ -16,27 +18,4 @@ fn main() {
         println!("\x1b[41m[!]Application error: {e}\x1b[0m");
         process::exit(1);
     };
-}
-//==================================================================
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text: \n {}", contents);
-    Ok(())
-}
-//===================================================================
-struct Config {
-    query: String,
-    file_path: String,
-}
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("\x1b[41m[!!!] Not enough arguments!\x1b[0m");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        
-        Ok(Config { query: query, file_path: file_path })
-    }
 }
